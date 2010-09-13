17cdf40434dc11c77c653e7ced5373e229570259
mixedpuppy <mixedpuppy@gmail.com>
2010-09-13 15:19:40 -0700

    merge update

--- linkdrop/lib/oauth/__init__.py
+++ linkdrop/lib/oauth/__init__.py
@@ -3,7 +3,7 @@ from linkdrop.lib.oauth import google_
 #from linkdrop.lib.oauth.live_ import LiveResponder
 #from linkdrop.lib.oauth.openidconsumer import OpenIDResponder
 from linkdrop.lib.oauth import twitter_
-#from linkdrop.lib.oauth.yahoo_ import YahooResponder
+from linkdrop.lib.oauth import yahoo_
 
 __all__ = ['get_provider']
 
@@ -11,7 +11,8 @@ __all__ = ['get_provider']
 _providers = {
     twitter_.domain:  twitter_,
     facebook_.domain: facebook_,
-    google_.domain: google_
+    google_.domain: google_,
+    yahoo_.domain: yahoo_
 }
 
 def get_provider(provider):
