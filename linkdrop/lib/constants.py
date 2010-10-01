525946aea2508d9fc2fffa297583a9fdaaf21fae
mixedpuppy <mixedpuppy@gmail.com>
2010-10-01 11:42:02 -0700

    add missing file

---
+++ linkdrop/lib/constants.py
@@ -0,0 +1,11 @@
+
+
+# json-rpc error codes, we'll use these since the error response object
+# is basically json-rpc style.
+PARSE_ERROR = -32700
+INVALID_REQUEST = -32600
+METHOD_NOT_FOUND = -32601
+INVALID_PARAMS = -32602
+INTERNAL_ERROR = -32603
+
+
