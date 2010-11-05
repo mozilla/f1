9c214bd24718bed03d456e9f0940be4dcddf43ab
Shane Caraveo <shanec@mayauel.(none)>
2010-11-05 14:57:44 -0700

    additional params for stats queries

--- linkdrop/controllers/stats.py
+++ linkdrop/controllers/stats.py
@@ -1,7 +1,12 @@
 import logging
+from datetime import timedelta
+import dateutil.parser
 
 from pylons import request, response, session, tmpl_context as c, url
 from pylons.controllers.util import abort, redirect
+
+from sqlalchemy.sql import select, func
+from sqlalchemy import and_
 
 from linkdrop.lib.helpers import json_exception_response, api_response, api_entry, api_arg
 
@@ -9,6 +14,7 @@ from linkdrop.lib.base import BaseController, render
 from linkdrop.model.meta import Session
 from linkdrop.model.account import Account
 from linkdrop.model.history import History
+from linkdrop.model.types import UTCDateTime
 
 log = logging.getLogger(__name__)
 
@@ -27,23 +33,51 @@ Get statistics about accounts on F1.
 """,
         queryargs=[
             # name, type, required, default, allowed, doc
-            api_arg('opts', 'string', False, None, None, """
+            api_arg('start', 'date', False, None, None, """
+start date (mm-dd-yyyy)
+"""),
+            api_arg('end', 'date', False, None, None, """
+end date (mm-dd-yyyy)
+"""),
+            api_arg('days', 'integer', False, 30, None, """
+number of days to return
+"""),
+            api_arg('opts', 'string', False, 'domain', None, """
 comma seperated values that can be [domain,perday]
 """),
         ],
         response={'type': 'list', 'doc': 'raw data list'}
     )
     def accounts(self):
-        limit = int(request.params.get('limit','5'))
+        start = request.params.get('start',None)
+        end = request.params.get('end',None)
+        limit = int(request.params.get('days','0'))
         opts = request.params.get('opts','').split(',')
-        if 'domain' in opts:
-            if 'perday' in opts:
-                sql = 'SELECT DATE(created), domain, count(id) FROM accounts a group by TO_DAYS(created), domain'
+        groupby = []
+        whereclause = []
+        if limit and not start and not end:
+            whereclause.append(Account.created >= UTCDateTime.now() - timedelta(days=limit))
+        if start:
+            whereclause.append(Account.created >= UTCDateTime.from_string(start))
+        if end:
+            whereclause.append(Account.created < UTCDateTime.from_string(end))
+        if 'perday' in opts:
+            if 'domain' in opts:
+                s = select([func.date(Account.created), Account.domain, func.count(Account.id)])
+                groupby.append(func.to_days(Account.created))
+                groupby.append(Account.domain)
             else:
-                sql = 'SELECT COUNT(domain), domain from accounts group by domain'
+                s = select([func.date(Account.created), func.count(Account.id)])
+                groupby.append(func.to_days(Account.created))
         else:
-            sql = 'SELECT DATE(created), count(id) FROM accounts a group by TO_DAYS(created)'
-        return [list(a) for a in Session.execute(sql).fetchall()]
+            s = select([func.count(Account.domain), Account.domain])
+            groupby.append(Account.domain)
+
+        if whereclause:
+            s = s.where(*whereclause)
+        if groupby:
+            s = s.group_by(*groupby)
+        return [list(a) for a in Session.execute(s).fetchall()]
 
     # for now do not enable this
     #@api_response
@@ -68,22 +102,51 @@ Get statistics about shares on F1.
 """,
         queryargs=[
             # name, type, required, default, allowed, doc
-            api_arg('opts', 'string', False, None, None, """
+            api_arg('start', 'date', False, None, None, """
+start date (mm-dd-yyyy)
+"""),
+            api_arg('end', 'date', False, None, None, """
+end date (mm-dd-yyyy)
+"""),
+            api_arg('days', 'integer', False, 30, None, """
+number of days to return
+"""),
+            api_arg('opts', 'string', False, 'domain', None, """
 comma seperated values that can be [domain,perday]
 """),
         ],
         response={'type': 'list', 'doc': 'raw data list'}
     )
     def history(self):
-        limit = int(request.params.get('limit','5'))
+        start = request.params.get('start',None)
+        end = request.params.get('end',None)
+        limit = int(request.params.get('days','0'))
         opts = request.params.get('opts','').split(',')
-        if 'domain' in opts:
-            if 'perday' in opts:
-                sql = 'SELECT DATE_FORMAT(published, "%Y-%m-%d"), domain, count(domain) FROM history a group by TO_DAYS(published), domain'
+        whereclause = []
+        vars = {}
+        groupby = []
+        if limit and not start and not end:
+            whereclause.append(History.published >= UTCDateTime.now() - timedelta(days=limit))
+        if start:
+            whereclause.append(History.published >= UTCDateTime.from_string(start))
+        if end:
+            whereclause.append(History.published < UTCDateTime.from_string(end))
+        if 'perday' in opts:
+            if 'domain' in opts:
+                s = select([func.date_format(History.published, "%Y-%m-%d"), History.domain, func.count(History.domain)],)
+                groupby.append(func.to_days(History.published))
+                groupby.append(History.domain)
             else:
-                sql = 'SELECT COUNT(domain), domain from history group by domain'
+                s = select([func.date_format(History.published, "%Y-%m-%d"), func.count(History.id)])
+                groupby.append(func.to_days(History.published))
         else:
-            sql = 'SELECT DATE_FORMAT(published, "%Y-%m-%d"), count(id) FROM history a group by TO_DAYS(published)'
-        return [list(a) for a in Session.execute(sql).fetchall()]
+            s = select([func.count(History.domain), History.domain])
+            groupby.append(History.domain)
+        
+        if whereclause:
+            s = s.where(and_(*whereclause))
+        if groupby:
+            s = s.group_by(*groupby)
+        return [list(a) for a in Session.execute(s).fetchall()]
 
 
