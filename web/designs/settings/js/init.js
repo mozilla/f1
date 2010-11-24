27b6b202a0e108a8183b6be77c51f4332045819f
Andy Chung <animalyouth@gmail.com>
2010-11-23 17:04:47 -0800

    designs for bryans settings in a new tab

--- web/designs/settings/js/init.js
+++ web/designs/settings/js/init.js
@@ -4,6 +4,34 @@ $(document).ready(function($) {
     $(function() {
         $(".overflow").textOverflow(null,true);
     });
+    
+    // tabs
+    $("#settings").hide();
+    
+    $("ul#tabs li").click(function() {
+        $(this).addClass("selected");
+        $(this).siblings().removeClass("selected");
+    });
+
+    $("ul#tabs li.manage").click(function() {
+        if ($("#manage").is(":hidden")) {
+            $("#manage").fadeIn(200);
+            $("#manage").siblings().fadeOut(0);
+        }
+        else {
+            $("#manage").noop();
+        }
+    });
+    
+    $("ul#tabs li.settings").click(function() {
+        if ($("#settings").is(":hidden")) {
+            $("#settings").fadeIn(200);
+            $("#settings").siblings().fadeOut(0);
+        }
+        else {
+            $("#settings").noop();
+        }
+    });
 
     // done!
 });  
