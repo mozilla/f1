ec8021d77567d70bf85a7f297bf0cf61be243df5
Anant Narayanan <anant@kix.in>
2011-03-14 16:50:30 -0700

    Fix reference to forceReload and define progress listeners

--- extensions/firefox-share/src/modules/progress.js
+++ extensions/firefox-share/src/modules/progress.js
@@ -1,13 +1,59 @@
-const { interfaces: Ci } = Components;
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 const errorPage = "resource://f1/chrome/content/down.html";
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 // This progress listener looks for HTTP codes that are errors/not
 // successses and puts up the "server down" page bundled in the extension.
 // This listener is related to the HttpActivityObserver, but that one handles
 // cases when the server is just not reachable via the network. This one
 // handles the cases where the server is reachable but is freaking out.
-function StateProgressListener(browser) {
+function StateProgressListener(browser, panel) {
+    this.panel = panel;
     this.browser = browser;
+
+    this.firstRunProgressListener = {
+        QueryInterface: XPCOMUtils.generateQI(
+            [Ci.nsIWebProgressListener, Ci.nsISupportsWeakReference, Ci.nsISupports]
+        ),
+
+        onStateChange: function (aWebProgress, aRequest, aStateFlags, aStatus) {
+            // maybe can just use onLocationChange, but I don't think so?
+            let flags = Ci.nsIWebProgressListener;
+            // This seems like an excessive check but works very well
+            if (aStateFlags & flags.STATE_IS_WINDOW && aStateFlags & flags.STATE_STOP) {
+                if (!ffshare.didOnFirstRun) {
+                    //Be sure to disable first run after one try. Even if it does
+                    //not work, do not want to annoy the user with continual popping up
+                    //of the front page.
+                    ffshare.didOnFirstRun = true;
+                    ffshare.onFirstRun();
+                }
+            }
+        },
+
+        onLocationChange: function (aWebProgress, aRequest, aLocation) {},
+        onProgressChange: function (aWebProgress, aRequest, aCurSelfProgress, aMaxSelfProgress, aCurTotalProgress, aMaxTotalProgress) {},
+        onSecurityChange: function (aWebProgress, aRequest, aState) {},
+        onStatusChange: function (aWebProgress, aRequest, aStatus, aMessage) {}
+    };
+
+    this.canShareProgressListener = {
+        QueryInterface: XPCOMUtils.generateQI(
+            [Ci.nsIWebProgressListener, Ci.nsISupportsWeakReference, Ci.nsISupports]
+        ),
+
+        onLocationChange: function (aWebProgress, aRequest, aLocation) {
+            ffshare.canShareURI(aLocation);
+            ffshare._window.setTimeout(function () {
+                ffshare.switchTab(true);
+            }, 0);
+        },
+
+        onStateChange: function (aWebProgress, aRequest, aStateFlags, aStatus) {},
+        onProgressChange: function (aWebProgress, aRequest, aCurSelfProgress, aMaxSelfProgress, aCurTotalProgress, aMaxTotalProgress) {},
+        onSecurityChange: function (aWebProgress, aRequest, aState) {},
+        onStatusChange: function (aWebProgress, aRequest, aStatus, aMessage) {}
+    };
 }
 StateProgressListener.prototype = {
     // detect communication from the iframe via location setting
@@ -21,13 +67,13 @@ StateProgressListener.prototype = {
     },
 
     onStateChange: function (aWebProgress, aRequest, aStateFlags, aStatus) {
-        var flags = Ci.nsIWebProgressListener;
+        let flags = Ci.nsIWebProgressListener;
         
         if (!('nsIHttpChannel' in aRequest))
             return;
         
         if (aStateFlags & flags.STATE_IS_WINDOW && aStateFlags & flags.STATE_STOP) {
-            var status;
+            let status;
             try {
                 status = aRequest.nsIHttpChannel.responseStatus;
             } catch (e) {
@@ -36,7 +82,7 @@ StateProgressListener.prototype = {
                 // Also ignore this state for about:blank which is apparently used as
                 // a placeholder by FF while first creating the panel/browser element.
                 // Check against channel.name, that is what we were *trying* to load.
-                var href = aRequest.nsIHttpChannel.name;
+                let href = aRequest.nsIHttpChannel.name;
                 if (href !== errorPage && href !== 'about:blank') {
                     status = 1000;
                 } else {
@@ -46,7 +92,7 @@ StateProgressListener.prototype = {
 
             if (status < 200 || status > 399) {
                 this.browser.contentWindow.location = errorPage;
-                forceReload = true;
+                this.panel.forceReload = true;
             }
         }
     },
@@ -55,7 +101,7 @@ StateProgressListener.prototype = {
     onProgressChange: function (aWebProgress, aRequest, aCurSelfProgress, aMaxSelfProgress, aCurTotalProgress, aMaxTotalProgress) {},
     onSecurityChange: function (aWebProgress, aRequest, aState) {},
     onStatusChange: function (aWebProgress, aRequest, aStatus, aMessage) {
-        //log("onStatus Change: " + aRequest.nsIHttpChannel.responseStatus + ": " + aRequest.loadFlags + ", " + aRequest + ", " + aMessage);
+        //dump("onStatus Change: " + aRequest.nsIHttpChannel.responseStatus + ": " + aRequest.loadFlags + ", " + aRequest + ", " + aMessage);
     }
 };
 
