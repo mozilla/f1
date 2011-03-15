9d06bdc5272d64e10fa8b14f7db96bbf8721faa7
Shane Caraveo <mixedpuppy@gmail.com>
2011-03-15 16:10:07 -0700

    update panel to the latest code from develop

--- extensions/firefox-share/src/modules/panel.js
+++ extensions/firefox-share/src/modules/panel.js
@@ -164,6 +164,7 @@ sharePanel.prototype = {
       title: this.getPageTitle(),
       description: this.getPageDescription(),
       medium: this.getPageMedium(),
+      source: this.getSourceURL(),
       url: this.gBrowser.currentURI.spec,
       canonicalUrl: this.getCanonicalURL(),
       shortUrl: this.getShortURL(),
@@ -178,24 +179,21 @@ sharePanel.prototype = {
     return options;
   },
 
-
-  // PostMessage APIs
-  // Called by content page when share is successful.
-  // @param {Object} data info about the share.
+  /**
+   * PostMessage APIs
+   * Called by content page when share is successful.
+   * @param {Object} data info about the share.
+   */
   success: function (data) {
-    this.updateStatus(0);
+    this.updateStatus([SHARE_DONE,,,data.url], true);
     this.close();
 
+    // XXX we should work out a better bookmarking system
+    // https:// github.com/mozilla/f1/issues/66
     if (this.ffshare.prefs.bookmarking) {
-      let tags = ['shared', 'f1'];
-      if (data.domain === 'twitter.com') {
-        tags.push("twitter");
-      }
-      if (data.domain === 'facebook.com') {
-        tags.push("facebook");
-      }
+      let tags = ['shared', 'f1', data.service];
 
-      let nsiuri = Services.io.newURI(this.gBrowser.currentURI.spec, null, null);
+      let nsiuri = Services.io.newURI(data.url, null, null);
       Services.bookmarks.insertBookmark(
         Services.bookmarks.unfiledBookmarksFolder, nsiuri,
         Services.bookmarks.DEFAULT_INDEX, this.getPageTitle().trim()
@@ -205,7 +203,8 @@ sharePanel.prototype = {
   },
 
   getPageTitle: function () {
-    let metas = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:title']"), i, title, content;
+    let metas = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:title']"),
+        i, title, content;
     for (i = 0; i < metas.length; i++) {
       content = metas[i].getAttribute("content");
       if (content) {
@@ -233,7 +232,8 @@ sharePanel.prototype = {
   },
 
   getPageDescription: function () {
-    let metas = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:description']"), i, content;
+    let metas = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:description']"),
+        i, content;
     for (i = 0; i < metas.length; i++) {
       content = metas[i].getAttribute("content");
       if (content) {
@@ -265,8 +265,8 @@ sharePanel.prototype = {
   // According to Facebook - (only the first 3 are interesting)
   // Valid values for medium_type are audio, image, video, news, blog, and mult.
   getPageMedium: function () {
-    let metas =
-    this.gBrowser.contentDocument.querySelectorAll("meta[property='og:type']"), i, content;
+    let metas = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:type']"),
+        i, content;
     for (i = 0; i < metas.length; i++) {
       content = metas[i].getAttribute("content");
       if (content) {
@@ -284,40 +284,113 @@ sharePanel.prototype = {
     return "";
   },
 
+  getSourceURL: function () {
+    // Ideally each page would report the medium correctly, but some
+    // do not, like vimeo, so always just look for a video source.
+    let source = this.getVideoSourceURL();
+    return source || "";
+  },
+
+  getVideoSourceURL: function () {
+    let metas = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:video']");
+    for (let i = 0; i < metas.length; i++) {
+      let content = metas[i].getAttribute("content");
+      if (content && (content = this._validURL(unescapeXml(content)))) {
+        return content;
+      }
+    }
+    return this.getVideoSourceURLHacks();
+  },
+
+  getVideoSourceURLHacks: function () {
+    let canonical = this.getCanonicalURL(),
+        host = this.gBrowser.currentURI.host,
+        params, embeds, i, src, flashvars, value, url;
+
+    // YouTube hack to get the right source without too many parameters
+    if (host.indexOf("youtube.com") >= 0 &&
+        canonical.match(/v=([A-Za-z0-9._%\-]*)[&\w;=\+_\-]*/)) {
+      let id = canonical.match(/v=([A-Za-z0-9._%\-]*)[&\w;=\+_\-]*/)[1];
+      return "http:// www.youtube.com/v/" + id;
+    }
+
+    // Vimeo hack to find the <object data="src"><param name="flashvars"/></object> pieces we need
+    embeds = this.gBrowser.contentDocument.querySelectorAll("object[type='application/x-shockwave-flash'][data]");
+    params = this.gBrowser.contentDocument.querySelectorAll("param[name='flashvars']");
+    if (params && params.length) {
+      for (i = 0; i < embeds.length; i++) {
+        src = embeds[i].getAttribute("data");
+        flashvars = params[0].getAttribute("value");
+        if (flashvars) {
+          src += (src.indexOf("?") < 0 ? "?" : "&amp;") + decodeURIComponent(flashvars);
+        }
+        if ((url = this._validURL(unescapeXml(src)))) {
+          return url;
+        }
+      }
+    }
+
+    // A generic hack that looks for the <param name="movie"> which is often available
+    // for backwards compat and IE
+    params = this.gBrowser.contentDocument.querySelectorAll("param[name='movie']");
+    for (i = 0; i < params.length; i++) {
+      value = params[i].getAttribute("value");
+      if (value) {
+        if ((url = this._validURL(unescapeXml(value)))) {
+          return url;
+        }
+      }
+    }
+
+    // This one is fairly bad because the flashvars can exceed a reasonable
+    // url length limit and since it is only sent to flash it is often large
+    embeds = this.gBrowser.contentDocument.querySelectorAll("embed[src]");
+    for (i = 0; i < embeds.length; i++) {
+      src = embeds[i].getAttribute("src");
+      flashvars = embeds[i].getAttribute("flashvars");
+      if (flashvars) {
+        src += (src.indexOf("?") < 0 ? "?" : "&amp;") + decodeURIComponent(flashvars);
+      }
+      if ((url = this._validURL(unescapeXml(src)))) {
+        return url;
+      }
+    }
+    return "";
+  },
+
   getShortURL: function () {
-    let shorturl = this.gBrowser.contentDocument.getElementById("shorturl")
-    let links = this.gBrowser.contentDocument.querySelectorAll("link[rel='shortlink']");
+    let shorturl = this.gBrowser.contentDocument.getElementById("shorturl"),
+        links = this.gBrowser.contentDocument.querySelectorAll("link[rel='shortlink']");
 
     // flickr does id="shorturl"
-    if (shorturl) {
-      return shorturl.getAttribute("href");
+    if (shorturl && (shorturl = this._validURL(shorturl.getAttribute("href")))) {
+      return shorturl;
     }
 
     for (let i = 0; i < links.length; i++) {
-      let content = links[i].getAttribute("href");
+      let content = this._validURL(links[i].getAttribute("href"));
       if (content) {
-        return this.gBrowser.currentURI.resolve(content);
+        return content;
       }
     }
     return "";
   },
 
   getCanonicalURL: function () {
-    let links = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:url']"), i, content;
+    let links = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:url']"),
+        i, content;
 
     for (i = 0; i < links.length; i++) {
-      content = links[i].getAttribute("content");
-      if (content) {
-        return this.gBrowser.currentURI.resolve(content);
+      if ((content = this._validURL(links[i].getAttribute("content")))) {
+        return content;
       }
     }
 
     links = this.gBrowser.contentDocument.querySelectorAll("link[rel='canonical']");
 
     for (i = 0; i < links.length; i++) {
-      content = links[i].getAttribute("href");
-      if (content) {
-        return this.gBrowser.currentURI.resolve(content);
+      if ((content = this._validURL(links[i].getAttribute("href")))) {
+        return content;
       }
     }
 
@@ -330,7 +403,7 @@ sharePanel.prototype = {
   getCanonicalURLHacks: function () {
     // Google Maps Hack :( obviously this regex isn't robust
     if (/^maps\.google\.[a-zA-Z]{2,5}/.test(this.gBrowser.currentURI.host)) {
-      return this.gBrowser.contentDocument.getElementById("link").getAttribute("href");
+      return this._validURL(this.gBrowser.contentDocument.getElementById("link").getAttribute("href"));
     }
 
     return '';
@@ -372,14 +445,17 @@ sharePanel.prototype = {
    * originating from the share UI in content-space
    */
   generateBase64Preview: function (imgUrl) {
+    let self = this;
     let img = new this.browser.contentWindow.Image();
-    img.onload = fn.bind(this, function () {
+    img.onload = function () {
 
-      let canvas = this.gBrowser.contentDocument.createElement("canvas");
-      let win = this.browser.contentWindow.wrappedJSObject;
-      let w = img.width, h = img.height, dataUrl, canvasW, canvasH, ctx, scale;
+      let canvas = self.gBrowser.contentDocument.createElement("canvas"),
+          win = self.browser.contentWindow.wrappedJSObject,
+          w = img.width,
+          h = img.height,
+          dataUrl, canvasW, canvasH, ctx, scale;
 
-      //Put upper constraints on the image size.
+      // Put upper constraints on the image size.
       if (w > 10000) {
         w = 10000;
       }
@@ -407,49 +483,71 @@ sharePanel.prototype = {
         data: dataUrl
       }), win.location.protocol + "//" + win.location.host);
 
-    });
+    };
     img.src = imgUrl;
 
   },
 
+  _validURL: function(url) {
+    // hacky validation of a url to make sure it at least appears valid
+    url = this.gBrowser.currentURI.resolve(url);
+    if (!/\w+:\/\/[^\/]+\/.+/.test(url))
+      return null;
+    return url;
+  },
+  
   previews: function () {
     // Look for FB og:image and then rel="image_src" to use if available
     // for og:image see: http://developers.facebook.com/docs/share
     // for image_src see: http://about.digg.com/thumbnails
-    let metas = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:image']")
-    let links = this.gBrowser.contentDocument.querySelectorAll("link[rel='image_src']")
-    let previews = [], i, content;
+    let metas = this.gBrowser.contentDocument.querySelectorAll("meta[property='og:image']"),
+        links = this.gBrowser.contentDocument.querySelectorAll("link[rel='image_src']"),
+        previews = [], i, content;
 
     for (i = 0; i < metas.length; i++) {
-      content = metas[i].getAttribute("content");
+      content = this._validURL(metas[i].getAttribute("content"));
       if (content) {
         previews.push({
-          http_url: this.gBrowser.currentURI.resolve(content),
-          base64: ""
+          http_url : content,
+          base64 : ""
         });
       }
     }
 
     for (i = 0; i < links.length; i++) {
-      content = links[i].getAttribute("href");
+      content = this._validURL(links[i].getAttribute("href"));
       if (content) {
         previews.push({
-          http_url: this.gBrowser.currentURI.resolve(content),
-          base64: ""
+          http_url : content,
+          base64 : ""
         });
       }
     }
 
     // Push in the page thumbnail last in case there aren't others
-    previews.push({
-      http_url: "",
-      base64: this.getThumbnailData()
-    });
+    previews.push(
+      {
+        http_url : "",
+        base64 : this.getThumbnailData()
+      }
+    );
     return previews;
   },
 
+  getShareState: function() {
+    let win = this.browser.contentWindow.wrappedJSObject;
+    win.postMessage(JSON.stringify({
+        topic: 'shareState',
+        data: this.gBrowser.selectedTab.shareState
+      }), win.location.protocol + "//" + win.location.host);
+  },
+
   escapeHtml: function (text) {
-    return text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+    return text
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;');
   },
 
   sizeToContent: function () {
@@ -470,50 +568,101 @@ sharePanel.prototype = {
   },
 
 
-  // panelUI operations
+  /**
+   * Called when we want to hide the panel and possibly destroy the shareState information
+   */
   close: function () {
     this.panel.hidePopup();
     if (this.gBrowser.selectedTab.shareState) {
-      if (this.gBrowser.selectedTab.shareState.status === 0) this.gBrowser.selectedTab.shareState = null;
-      else
-      this.gBrowser.selectedTab.shareState.open = false;
+      if (this.gBrowser.selectedTab.shareState.status === 0) {
+        this.gBrowser.selectedTab.shareState = null;
+      } else {
+        this.gBrowser.selectedTab.shareState.open = false;
+      }
     }
 
     // Always ensure the button is unchecked when the panel is hidden
     if (this.button) this.button.removeAttribute("checked");
   },
 
-  updateStatus: function (status) {
-    let self = this;
-    if (typeof(status) == 'undefined') status = this.gBrowser.selectedTab.shareState ? this.gBrowser.selectedTab.shareState.status : 0;
-    if (this.gBrowser.selectedTab.shareState) this.gBrowser.selectedTab.shareState.status = status;
-    if (status == 2) {
-      // use the notification bar if the button is not in the urlbar
-      let nBox = this.gBrowser.getNotificationBox();
-      let buttons = [
-        {
-        label: "try again",
-        accessKey: null,
-        callback: function () {
-          self.gBrowser.getNotificationBox().removeCurrentNotification();
-          self.window.setTimeout(function () {
-            self.ffshare.togglePanel();
-          }, 0);
-        }
-      }];
-      nBox.appendNotification(
-        this.ffshare._getString("ffshareProblem.status"),
-        this.ffshare._getString("ffshareProblem.code"),
-        null, nBox.PRIORITY_WARNING_MEDIUM, buttons
-      );
+  getBrowserTabForUrl: function(url) {
+    if (!url)
+      return this.gBrowser.selectedTab;
+    if (this.gBrowser.getBrowserForTab(this.gBrowser.selectedTab).currentURI.spec == url)
+      return this.gBrowser.selectedTab;
+    var numTabs = this.gBrowser.browsers.length;
+    for (var index = 0; index < numTabs; index++) {
+      var currentBrowser = this.gBrowser.getBrowserAtIndex(index);
+      if (url == currentBrowser.currentURI.spec) {
+        return this.gBrowser.tabs[index];
+      }
+    }
+    return this.gBrowser.selectedTab;
+  },
+
+  /**
+   * Updates the state of the toolbar button during a share activity or
+   * afterward when a share error is received.
+   * @param {Integer} an index value that has meaning in the SHARE_STATUS array
+   * @param {Boolean} only passed by the final success call
+   */
+  updateStatus: function (statusData, success) {
+    let contentTab = this.getBrowserTabForUrl(statusData && statusData.length > 3 ? statusData[3] : null),
+        nBox = this.gBrowser.getNotificationBox(this.gBrowser.getBrowserForTab(contentTab)),
+        notification = nBox.getNotificationWithValue("mozilla-f1-share-error"),
+        button = this.button,
+        self = this,
+        status,
+        buttons;
+
+    if (typeof(statusData) === 'undefined') {
+      statusData = contentTab.shareState ? contentTab.shareState.status : [SHARE_DONE];
+    }
+    status = statusData[0];
+
+    if (status === SHARE_ERROR) {
+      // Check that we aren't already displaying our notification
+      if (!notification) {
+        buttons = [{
+          label: "try again",
+          accessKey: null,
+          callback: function () {
+            let nb = self.gBrowser.getNotificationBox();
+            nb.removeNotification(nb.getNotificationWithValue("mozilla-f1-share-error"));
+            window.setTimeout(function () {
+              ffshare.togglePanel();
+            }, 0);
+          }
+        }];
+        nBox.appendNotification("There was a problem sharing this page.",
+                                "mozilla-f1-share-error",
+                                null,
+                                nBox.PRIORITY_WARNING_MEDIUM, buttons);
+      }
+    } else if (status === SHARE_DONE && notification) {
+      nBox.removeNotification(notification);
     }
 
-    if (this.button) {
-      if (status == 2) status = 0;
-      this.button.setAttribute("status", SHARE_STATUS[status]);
+    if (contentTab.shareState) {
+      contentTab.shareState.status = statusData;
+    }
+
+    if (button && this.gBrowser.selectedTab == contentTab) {
+      // Only a final successful share should be passing this value
+      if (success) {
+        button.setAttribute("status", SHARE_STATUS[SHARE_FINISHED]);
+        window.setTimeout(function () {
+          button.setAttribute("status", SHARE_STATUS[status]);
+        }, 2900);
+      } else {
+        button.setAttribute("status", SHARE_STATUS[status]);
+      }
     }
   },
 
+  /**
+   * Called when we only want to hide the panel and preserve the shareState information
+   */
   hide: function () {
     this.panel.hidePopup();
     this.gBrowser.selectedTab.shareState.open = false;
@@ -521,11 +670,19 @@ sharePanel.prototype = {
   },
 
   show: function (options) {
-    let tabURI = this.gBrowser.getBrowserForTab(this.gBrowser.selectedTab).currentURI, tabUrl = tabURI.spec;
-    if (!this.ffshare.isValidURI(tabURI)) {
+    let contentBrowser = this.gBrowser.getBrowserForTab(this.gBrowser.selectedTab),
+        tabURI = contentBrowser.currentURI,
+        tabUrl = tabURI.spec,
+        nBox = this.gBrowser.getNotificationBox(contentBrowser),
+        notification = nBox.getNotificationWithValue("mozilla-f1-share-error");
+
+    if (!ffshare.isValidURI(tabURI)) {
       return;
     }
 
+    if (notification) {
+      nBox.removeNotification(notification);
+    }
     let currentState = this.gBrowser.selectedTab.shareState;
     options = this.getOptions(options);
 
@@ -565,7 +722,7 @@ sharePanel.prototype = {
     }
 
     // fx 4
-    let position = 'bottomcenter topleft';
+    let position = (getComputedStyle(gNavToolbox, "").direction === "rtl") ? 'bottomcenter topright' : 'bottomcenter topleft';
     this.panel.openPopup(anchor, position, 0, 0, false, false);
   }
 };
