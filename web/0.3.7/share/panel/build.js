0c749f108fb1769b305c46d2148a44ec4fa688c9
jrburke <jrburke@gmail.com>
2011-04-12 16:15:01 -0700

    versioned resources for release

---
+++ web/0.3.7/share/panel/build.js
@@ -0,0 +1,13 @@
+({
+    baseUrl: "../../scripts/",
+    paths: {
+        "index": "../share/panel/index",
+        "jquery": "jqueryStub",
+        "widgets": "../share/panel/scripts/widgets"
+    },
+    name: "index",
+    include: ['widgets/AccountPanelLinkedIn', 'widgets/AccountPanelFaceBook',
+              'ContactsEmail', 'ContactsTwitter', 'ContactsLinkedIn'],
+    exclude: ['jquery', 'require/text'],
+    out: './index.js'
+})
